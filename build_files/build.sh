c44b6c369ac7e8d738e781722351690429dc40ef
Peter Robertz <peter@muchbetter.de>
2025-12-28 21:44:22 +0100

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,7 +9,7 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-sudo dnf copr enable avengemedia/dms
+sudo dnf -y copr enable avengemedia/dms
 sudo dnf install niri dms
 sudo dnf -y copr disable avengemedia/dms
 
