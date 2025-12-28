90376963c10462e2eab05b26b0d412816434d01c
Peter Robertz <peter@muchbetter.de>
2025-12-28 23:12:46 +0100

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -18,23 +18,23 @@ dnf install -y dms
 dnf -y copr disable avengemedia/dms
 
 # COSMIC via COPR installieren
-dnf -y copr enable ryanabx/cosmic-epoch
-dnf install -y cosmic-desktop
-dnf -y copr disable ryanabx/cosmic-epoch
+#dnf -y copr enable ryanabx/cosmic-epoch
+#dnf install -y cosmic-desktop
+#dnf -y copr disable ryanabx/cosmic-epoch
 
 # Scroll from source bauen und installieren
-dnf install -y meson ninja-build git lua-devel wayland-devel wayland-protocols-devel libdrm-devel libinput-devel libxkbcommon-devel pixman-devel mesa-libgbm-devel libseat-devel hwdata-devel systemd-devel json-c-devel pango-devel cairo-devel gobject-introspection-devel libdisplay-info-devel libliftoff-devel
-git clone https://github.com/dawsers/scroll.git /tmp/scroll
-cd /tmp/scroll
-meson setup build
-ninja -C build
-ninja -C build install
-cd -
-rm -rf /tmp/scroll
+#dnf install -y meson ninja-build git lua-devel wayland-devel wayland-protocols-devel libdrm-devel libinput-devel libxkbcommon-devel pixman-devel mesa-libgbm-devel libseat-devel hwdata-devel systemd-devel json-c-devel pango-devel cairo-devel gobject-introspection-devel libdisplay-info-devel libliftoff-devel
+#git clone https://github.com/dawsers/scroll.git /tmp/scroll
+#cd /tmp/scroll
+#meson setup build
+#ninja -C build
+#ninja -C build install
+#cd -
+#rm -rf /tmp/scroll
 
 # MangoWC via Terra Repository installieren
-dnf install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
-dnf install -y mangowc
+#dnf install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+#dnf install -y mangowc
 
 # DMS-Konfiguration für neue User
 git clone https://github.com/AvengeMedia/DankMaterialShell.git /etc/skel/.config/DankMaterialShell
@@ -48,16 +48,16 @@ mkdir -p /etc/skel/.config/niri
 echo 'spawn "dms"' >> /etc/skel/.config/niri/config.kdl
 
 # COSMIC-Config anpassen (DMS-Start, Kompatibilität prüfen)
-mkdir -p /etc/skel/.config/cosmic
-echo "exec-once = dms" >> /etc/skel/.config/cosmic/config.toml
+#mkdir -p /etc/skel/.config/cosmic
+#echo "exec-once = dms" >> /etc/skel/.config/cosmic/config.toml
 
 # Scroll-Config anpassen (DMS-Start, Kompatibilität prüfen)
-mkdir -p /etc/skel/.config/scroll
-echo "exec = dms" >> /etc/skel/.config/scroll/config
+#mkdir -p /etc/skel/.config/scroll
+#echo "exec = dms" >> /etc/skel/.config/scroll/config
 
 # MangoWC-Config anpassen (DMS-Start, Kompatibilität prüfen)
-mkdir -p /etc/skel/.config/mango
-echo "exec dms" >> /etc/skel/.config/mango/config
+#mkdir -p /etc/skel/.config/mango
+#echo "exec dms" >> /etc/skel/.config/mango/config
 
 # Beispiel: System Unit aktivieren
 systemctl enable podman.socket
