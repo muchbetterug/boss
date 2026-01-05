a8f723a88ab90f3806ff2503a17f6e32c452960e
Peter Robertz <peter@muchbetter.de>
2026-01-05 16:00:02 +0100

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,14 +3,14 @@
 set -ouex pipefail
 
 # DMS installieren
-dnf -y copr enable avengemedia/dms
-dnf install -y niri dms
-dnf -y copr disable avengemedia/dms
+dnf5 -y copr enable avengemedia/dms
+dnf5 install -y niri dms
+dnf5 -y copr disable avengemedia/dms
 
 systemctl --user add-wants niri.service dms
 
 # Podman Socket aktivieren
-systemctl enable podman.socket
+# systemctl enable podman.socket
 
 #change pretty name
 sed -i "s|^PRETTY_NAME=.*|PRETTY_NAME=\"boss\"|" /usr/lib/os-release
