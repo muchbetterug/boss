1402dba390878fafac1bcfb77779033c942d7f1f
Peter Robertz <peter@muchbetter.de>
2026-01-05 16:37:13 +0100

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,8 +4,14 @@ set -ouex pipefail
 
 # DMS installieren
 dnf5 -y copr enable avengemedia/dms
-dnf5 install -y niri dms
+dnf5 -y install niri dms
 dnf5 -y copr disable avengemedia/dms
+
+dnf5 -y copr enable zhangyi6324/noctalia-shell
+dnf5 -y install noctalia-shell
+dnf5 -y copr disable zhangyi6324/noctalia-shell
+
+noctalia-shell
 
 #systemctl --user add-wants niri.service dms
 
