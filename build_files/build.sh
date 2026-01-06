16e467f869cc25a38225a10111b975811de0998b
Peter Robertz <peter@muchbetter.de>
2026-01-06 22:31:54 +0100

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -29,5 +29,76 @@ dnf5 -y copr disable scottames/ghostty
 # 4) Extras
 dnf5 -y install kitty qt6ct
 
-# Terra-Release-Paket herunterladen und installieren
-dnf5 install -y https://repos.fyralabs.com/terra${FEDORA_VERSION}/terra-release-${FEDORA_VERSION}-1.noarch.rpm
+# 0) Build deps installieren
+# (Liste ist bewusst etwas "breiter", damit Meson nicht mitten im Build wegen fehlender -devel Pakete abbricht.)
+dnf5 -y install \
+  git \
+  gcc gcc-c++ \
+  meson ninja-build \
+  pkgconf-pkg-config \
+  wayland-devel wayland-protocols-devel \
+  libxkbcommon-devel \
+  pixman-devel \
+  libdrm-devel \
+  mesa-libEGL-devel mesa-libGLES-devel mesa-libgbm-devel \
+  libinput-devel \
+  libseat-devel \
+  systemd-devel \
+  xorg-x11-server-Xwayland \
+  libxcb-devel xcb-util-devel xcb-util-wm-devel xcb-util-renderutil-devel xcb-util-image-devel xcb-util-keysyms-devel \
+  cairo-devel pango-devel \
+  glib2-devel \
+  hwdata
+
+# 1) Build wlroots (pinned)
+# Doku: git clone -b 0.19.2 ... meson build -Dprefix=/usr ... ninja install :contentReference[oaicite:3]{index=3}
+rm -rf /tmp/mangowc-build
+mkdir -p /tmp/mangowc-build
+cd /tmp/mangowc-build
+
+git clone -b 0.19.2 --depth 1 https://gitlab.freedesktop.org/wlroots/wlroots.git
+cd wlroots
+meson setup build -Dprefix=/usr -Dbuildtype=release
+ninja -C build install
+cd ..
+
+# 2) Build scenefx (pinned)
+# Doku: git clone -b 0.4.1 ... meson build -Dprefix=/usr ... ninja install :contentReference[oaicite:4]{index=4}
+git clone -b 0.4.1 --depth 1 https://github.com/wlrfx/scenefx.git
+cd scenefx
+meson setup build -Dprefix=/usr -Dbuildtype=release
+ninja -C build install
+cd ..
+
+# 3) Build MangoWC
+# Doku: "Finally, compile the compositor itself." (Meson/Ninja analog) :contentReference[oaicite:5]{index=5}
+git clone --depth 1 https://github.com/DreamMaoMao/mangowc.git
+cd mangowc
+meson setup build -Dprefix=/usr -Dbuildtype=release
+ninja -C build install
+
+# 4) Cleanup: Build-Verzeichnis entfernen
+cd /
+rm -rf /tmp/mangowc-build
+
+# Optional: Build deps entfernen, um Image klein zu halten
+# (Wenn du später im Build noch mehr kompilierst, dann erst ganz am Ende entfernen.)
+dnf5 -y remove \
+  git \
+  gcc gcc-c++ \
+  meson ninja-build \
+  pkgconf-pkg-config \
+  wayland-devel wayland-protocols-devel \
+  libxkbcommon-devel \
+  pixman-devel \
+  libdrm-devel \
+  mesa-libEGL-devel mesa-libGLES-devel mesa-libgbm-devel \
+  libinput-devel \
+  libseatd-devel \
+  systemd-devel \
+  libxcb-devel xcb-util-devel xcb-util-wm-devel xcb-util-renderutil-devel xcb-util-image-devel xcb-util-keysyms-devel \
+  cairo-devel pango-devel \
+  glib2-devel \
+  hwdata || true
+
+dnf5 -y clean all
