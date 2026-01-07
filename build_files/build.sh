9f159e27ba8bf6484094766379424a45d66d46f4
Peter Robertz <peter@muchbetter.de>
2026-01-07 16:10:47 +0100

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-
 set -ouex pipefail
 
-# RELEASE="$(rpm -E %fedora)"
+RELEASE="$(rpm -E %fedora)"
 
 log() {
-	echo "=== $* ==="
+  echo "=== $* ==="
 }
 
-# if true, sddm will be installed as the display manager.
-# NOTE: NOT FULLY IMPLEMENTED AND UNTESTED, DO NOT USE YET
 USE_SDDM=FALSE
 
 #######################################################################
@@ -18,246 +15,208 @@ USE_SDDM=FALSE
 
 log "Enable Copr repos..."
 COPR_REPOS=(
-	erikreider/SwayNotificationCenter # for swaync
-	errornointernet/packages
-	heus-sueh/packages                # for matugen/swww, needed by hyprpanel
-	leloubil/wl-clip-persist
-	# pgdev/ghostty
-	solopasha/hyprland
-	tofik/sway
-	ulysg/xwayland-satellite
-	yalter/niri
+  erikreider/SwayNotificationCenter # swaync
+  errornointernet/packages
+  heus-sueh/packages                # matugen/swww (hyprpanel)
+  leloubil/wl-clip-persist
+  solopasha/hyprland
+  tofik/sway
+  ulysj/xwayland-satellite
+  yalter/niri
 )
+
 for repo in "${COPR_REPOS[@]}"; do
-	# Try to enable the repo, but don't fail the build if it doesn't support this Fedora version
-	if ! dnf5 -y copr enable "$repo" 2>&1; then
-		log "Warning: Failed to enable COPR repo $repo (may not support Fedora $RELEASE)"
-	fi
+  if ! dnf5 -y copr enable "$repo" 2>&1; then
+    log "Warning: Failed to enable COPR repo $repo (may not support Fedora $RELEASE)"
+  fi
 done
 
-# log "Enable terra repositories..."
-# Bazzite disabled this for some reason so lets re-enable it again
-# dnf5 config-manager setopt terra.enabled=1 terra-extras.enabled=1
-
 #######################################################################
-## Install Packages
+# Packages
 #######################################################################
 
-# Note that these fedora font packages are preinstalled in the
-# bluefin-dx image, along with the SymbolsNerdFont which doesn't
-# have an associated fedora package:
-#
-#   adobe-source-code-pro-fonts
-#   google-droid-sans-fonts
-#   google-noto-sans-cjk-fonts
-#   google-noto-color-emoji-fonts
-#   jetbrains-mono-fonts
-#
-# Because the nerd font symbols are mapped correctly, we can get
-# nerd font characters anywhere.
 FONTS=(
-	fira-code-fonts
-	fontawesome-fonts-all
-	google-noto-emoji-fonts
+  fira-code-fonts
+  fontawesome-fonts-all
+  google-noto-emoji-fonts
 )
 
-# Hyprland dependencies to be installed, based on
-# https://github.com/JaKooLit/Fedora-Hyprland/ with additions
-# from ml4w and other sources.
 HYPR_DEPS=(
-	aquamarine
-	aylurs-gtk-shell2
-	blueman
-	bluez
-	bluez-tools
-	brightnessctl
-	btop
-	cava
-	cliphist
-	# egl-wayland
-	eog
-	fuzzel
-	gnome-bluetooth
-	grim
-	grimblast
-	gvfs
-	hyprpanel
-	inxi
-	kvantum
-	# lib32-nvidia-utils
-	libgtop2
-	mako
-	matugen
-	mpv
-	# mpv-mpris
-	network-manager-applet
-	nodejs
-	# nvidia-dkms
-	# nvidia-utils
-	nwg-look
-	pamixer
-	pavucontrol
-	playerctl
-	# power-profiles-daemon
-	python3-pyquery
-	qalculate-gtk
-	qt5ct
-	qt6ct
-	slurp
-	swappy
-	swaync
-	swww
-	tumbler
-    rofi
-	upower
-	wallust
-	waybar
-	wget2
-	wireplumber
-	wl-clipboard
-	wl-clip-persist
-	wlogout
-	wlr-randr
-	xarchiver
-	xdg-desktop-portal-gtk
-	xdg-desktop-portal-hyprland
-	xwayland-satellite
-	yad
+  aquamarine
+  aylurs-gtk-shell2
+  blueman
+  bluez
+  bluez-tools
+  brightnessctl
+  btop
+  cava
+  cliphist
+  eog
+  fuzzel
+  gnome-bluetooth
+  grim
+  grimblast
+  gvfs
+  hyprpanel
+  inxi
+  kvantum
+  libgtop2
+  mako
+  matugen
+  mpv
+  network-manager-applet
+  nodejs
+  nwg-look
+  pamixer
+  pavucontrol
+  playerctl
+  python3-pyquery
+  qalculate-gtk
+  qt5ct
+  qt6ct
+  rofi
+  slurp
+  swappy
+  swaync
+  swww
+  tumbler
+  upower
+  wallust
+  waybar
+  wget2
+  wireplumber
+  wl-clipboard
+  wl-clip-persist
+  wlogout
+  wlr-randr
+  xarchiver
+  xdg-desktop-portal-gtk
+  xwayland-satellite
+  yad
 )
 
-# Hyprland ecosystem packages
 HYPR_PKGS=(
-	hyprland
-	hyprcursor
-	hyprpaper
-	hyprpicker
-	hypridle
-	hyprlock
-	hyprshot
-	xdg-desktop-portal-hyprland
-	hyprsunset
-	hyprutils
+  hyprland
+  hyprcursor
+  hyprpaper
+  hyprpicker
+  hypridle
+  hyprlock
+  hyprshot
+  hyprsunset
+  hyprutils
+  xdg-desktop-portal-hyprland
 )
 
-# Detect if we're on Bazzite (has KDE/Qt 6.10) or Bluefin (has GNOME/Qt 6.9)
-# These Qt-dependent packages only work on Bluefin currently due to Qt version mismatch
+# Qt-dependent extras only on non-bazzite
 if ! grep -qi "bazzite" /usr/lib/os-release 2>/dev/null; then
-	# Only add Qt-dependent packages on Bluefin
-	HYPR_PKGS+=(
-		hyprsysteminfo
-		hyprpolkitagent
-		hyprland-qt-support
-	)
+  HYPR_PKGS+=(
+    hyprsysteminfo
+    hyprpolkitagent
+    hyprland-qt-support
+  )
 fi
 
-# Niri and its dependencies from its default config.
-# commented out packages are already referenced in this file, OR they
-# are prebundled inside our parent image.
 NIRI_PKGS=(
-	niri
-	swaylock
-	# alacritty
-	# brightnessctl
-	# fuzzel
-	# mako
-	# waybar
-	# xwayland-satellite
-	# gnome-keyring
-	# wireplumber
-	# xdg-desktop-portal-gnome
-	# xdg-desktop-portal-gtk
+  niri
+  swaylock
 )
 
-# SDDM not set up properly yet, so this is just a placeholder.
-# For now you'll have to invoke Hyprland from the command line.
 SDDM_PACKAGES=()
 if [[ $USE_SDDM == TRUE ]]; then
-	SDDM_PACKAGES=(
-		sddm
-		sddm-breeze
-		sddm-kcm
-		qt6-qt5compat
-	)
+  SDDM_PACKAGES=(
+    sddm
+    sddm-breeze
+    sddm-kcm
+    qt6-qt5compat
+  )
 fi
 
-# chrome etc are installed as flatpaks. We generally prefer that
-# for most things with GUIs, and homebrew for CLI apps. This list is
-# only special GUI apps that need to be installed at the system level.
 ADDITIONAL_SYSTEM_APPS=(
-	alacritty
-
-	# ghostty is broken in Fedora 42 right now
-	# ghostty
-
-	kitty
-	kitty-terminfo
-
-	thunar
-	thunar-volman
-	thunar-archive-plugin
+  alacritty
+  kitty
+  kitty-terminfo
+  thunar
+  thunar-volman
+  thunar-archive-plugin
 )
 
-# we do all package installs in one rpm-ostree command
-# so that we create minimal layers in the final image
-log "Installing packages using dnf5..."
-dnf5 install --setopt=install_weak_deps=False -y \
-	"${FONTS[@]}" \
-	"${HYPR_DEPS[@]}" \
-	"${HYPR_PKGS[@]}" \
-	"${NIRI_PKGS[@]}" \
-	"${SDDM_PACKAGES[@]}" \
-	"${ADDITIONAL_SYSTEM_APPS[@]}"
+# Build deps for Hyprland/Hyprpm compilation (incl. NVIDIA)
+BUILD_DEPS=(
+  gcc gcc-c++
+  clang llvm
+  cmake meson
+  pkg-config
+  git
+  cpio
+  ninja
+  python3
 
-dnf5 -y --setopt=install_weak_deps=False install \
-  gcc gcc-c++ \
-  clang llvm \
-  cmake meson \
-  pkg-config \
-  git \
-  cpio \
-  ninja \
-  python3 \
-  wayland-devel \
-  wayland-protocols-devel \
-  libXcursor-devel \
-  libXrandr-devel \
-  libXinerama-devel \
-  libX11-devel \
-  libxcb-devel \
-  libXi-devel \
-  mesa-libGL-devel \
-  mesa-libEGL-devel \
-  mesa-libgbm-devel \
-  libdrm-devel \
-  cairo-devel \
-  pango-devel \
-  pixman-devel \
-  libjpeg-turbo-devel \
-  libpng-devel \
-  pipewire-jack-audio-connection-kit-devel \
-  libseat-devel \
-  dbus-devel \
-  systemd-devel \
-  libuuid-devel \
-  nvidia-driver \
-  nvidia-driver-devel \
-  egl-wayland \
-  libinput-devel \
-  xcb-util-wm-devel \
-  xcb-util-renderutil-devel \
+  wayland-devel
+  wayland-protocols-devel
+
+  libXcursor-devel
+  libXrandr-devel
+  libXinerama-devel
+  libX11-devel
+  libxcb-devel
+  libXi-devel
+
+  xwayland-devel
+
+  mesa-libGL-devel
+  mesa-libEGL-devel
+  mesa-libgbm-devel
+  libdrm-devel
+
+  cairo-devel
+  pango-devel
+  pixman-devel
+  libjpeg-turbo-devel
+  libpng-devel
+
+  pipewire-jack-audio-connection-kit-devel
+  libseat-devel
+  dbus-devel
+  systemd-devel
+  libuuid-devel
+
+  nvidia-driver
+  nvidia-driver-devel
+  egl-wayland
+
+  libinput-devel
+  xcb-util-wm-devel
+  xcb-util-renderutil-devel
   xcb-util-devel
+)
 
 #######################################################################
-### Disable repositeories so they aren't cluttering up the final image
+# Install Packages (single transaction)
+#######################################################################
+
+log "Installing packages using dnf5..."
+dnf5 install --setopt=install_weak_deps=False -y \
+  "${FONTS[@]}" \
+  "${HYPR_DEPS[@]}" \
+  "${HYPR_PKGS[@]}" \
+  "${NIRI_PKGS[@]}" \
+  "${SDDM_PACKAGES[@]}" \
+  "${ADDITIONAL_SYSTEM_APPS[@]}" \
+  "${BUILD_DEPS[@]}"
+
+#######################################################################
+# Disable COPR repos to reduce clutter
+#######################################################################
 
 log "Disable Copr repos to get rid of clutter..."
 for repo in "${COPR_REPOS[@]}"; do
-	dnf5 -y copr disable "$repo"
+  dnf5 -y copr disable "$repo"
 done
 
-# -----------------------------
+#######################################################################
 # Noctalia Shell (+ quickshell) via COPR
-# -----------------------------
+#######################################################################
 log "Enable COPR: zhangyi6324/noctalia-shell"
 dnf5 -y copr enable zhangyi6324/noctalia-shell
 
@@ -266,21 +225,16 @@ dnf5 --setopt=install_weak_deps=False -y install noctalia-shell quickshell
 dnf5 -y copr disable zhangyi6324/noctalia-shell
 
 #######################################################################
-### Enable Services
-
-# TODO: these need to be run at first boot, not during image build
-
-# Setting Thunar as the default file manager
-# xdg-mime default thunar.desktop inode/directory
-# xdg-mime default thunar.desktop application/x-wayland-gnome-saved-search
+# Enable Services (placeholder)
+#######################################################################
 
 if [[ $USE_SDDM == TRUE ]]; then
-	log "Installing sddm...."
-	for login_manager in lightdm gdm lxdm lxdm-gtk3; do
-		if sudo dnf list installed "$login_manager" &>>/dev/null; then
-			sudo systemctl disable "$login_manager" 2>&1 | tee -a "$LOG"
-		fi
-	done
-	systemctl set-default graphical.target
-	systemctl enable sddm.service
+  log "Installing sddm...."
+  for login_manager in lightdm gdm lxdm lxdm-gtk3; do
+    if sudo dnf list installed "$login_manager" &>>/dev/null; then
+      sudo systemctl disable "$login_manager" 2>&1 | tee -a "$LOG"
+    fi
+  done
+  systemctl set-default graphical.target
+  systemctl enable sddm.service
 fi
